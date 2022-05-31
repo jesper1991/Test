60c762d4cf53f57f474cd16ad256b55fcbf40666
Jesper Åkerberg <criperq@gmail.com>
2022-05-31 11:27:03 +0200

    update

--- Files.py
+++ Files.py
@@ -1,12 +1,12 @@
 
-f = open('data.txt', 'w') # Skapa fil i output mode, sparar i current dir ('w' = write, 'r' = read)
+f = open('data.txt', 'w')   # Skapa fil i output mode, sparar i current dir ('w' = write, 'r' = read)
 f.write('Hello\n')
 f.write('world\n')
-f.close() # Close to flush output buffers to disk
+f.close()                   # Close to flush output buffers to disk
 
-f = open('data.txt') # Öppna filen
-text = f.read() # Lagra texten som läses av i variabel text
-print(text) # Print variabel text som innehåller den avlästa texten
+f = open('data.txt')    # Öppna filen
+text = f.read()         # Lagra texten som läses av i variabel text
+print(text)             # Print variabel text som innehåller den avlästa texten
 
 # Kan splitta texten i filen
 text.split()
@@ -14,8 +14,8 @@ text.split()
 # Skriva text i en fil utan 
 for line in open('data.txt'): print (line)
 
-dir(f) # See all methods on file
-help(f.seek) # See information about a method
+dir(f)          # See all methods on file
+help(f.seek)    # See information about a method
 
 # Binary Byte Files
 
@@ -34,9 +34,30 @@ print(data)
 # Går att slica
 data[4:8]
 
-list(data) # 115, 112, 97, 109 blir t.ex. spam i Ascii.
+list(data)       # 115, 112, 97, 109 blir t.ex. spam i Ascii.
 
 # Unpack till objekt igen
 struct.unpack('>i4sh', data)
 
 # UNICODE TEXT FILES
+
+S = 'sp\xc4m'   # Non-ASCII Unicode text
+print(S)        # spÄm
+
+file = open('unidata.txt', 'w', encoding='utf-8')
+file.write(S)
+file.close()
+
+text = open('unidata.txt', encoding='utf-8').read()
+print(text)
+
+
+raw = open('unidata.txt', 'rb').read()  # För att se raw encoded bytes
+raw
+raw.decode('utf-8')                     # Decode to str
+
+text.encode('latin-1')  # b'sp\xc4m
+text.encode('utf-16')   # b'\xff\xfes\x00p\x00\xc4\x00m\x00'
+
+
+
